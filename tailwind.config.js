4c83ee5f0d9eb0990ed7e99c0615f34059ea9b35
Irfan Alam <108110097+Irfanlam123@users.noreply.github.com>
2025-06-09 15:12:00 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // For React
+    "./public/index.html",        // If you use index.html
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
